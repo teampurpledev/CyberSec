9406d16ba1afb49e41070767609b10410e75ff67
teampurpledev <teampurple.dev@gmail.com>
2024-03-18 19:17:59 -0400

    chore: update echo to better reflect what code is attempting to do

--- network_monitoring_via_email/client/copy_http_log.sh
+++ network_monitoring_via_email/client/copy_http_log.sh
@@ -10,6 +10,6 @@ destination_path="/path/to/destination/http.log"
 
 # Make sure you have passwordless SSH setup between the machines to enable scp to work without manual authentication.
 # If passwordless SSH is not set up, you'll need to enter the password during the execution of the script.
+echo "Copying $source_machine:$source_path to $destination_machine $destination_path"
 scp "$source_machine:$source_path" - >> "$destination_path"
 
-echo "Log file copied and appended to http.log"
