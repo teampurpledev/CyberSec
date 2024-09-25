8e5d5f3a3f04593af71c2ac008dccdf09f3e18b3
teampurpledev <teampurple.dev@gmail.com>
2024-09-25 12:08:12 -0400

    Add EICAR test file download script to datagen

---
+++ datagen/downloadEICAR.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download the latest anti malware testfile from EICAR
+curl -O 'https://secure.eicar.org/eicar.com.txt'
+
+# For other file types see source at
+# https://www.eicar.org/download-anti-malware-testfile/
